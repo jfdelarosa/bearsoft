5a3da2291f9d317e04974c7f3120f30a0ee611a5
Juan Fernando <jfdelarosa@outlook.com>
2018-05-18 01:48:16 -0500

    no parallax

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,8 +1,6 @@
-import parallax from "./modules/parallax";
 import scrollTo from "./modules/scrollTo";
 import formSubmit from "./modules/form";
 
-parallax();
 document.querySelectorAll(".nav__element").forEach((el) => {
   el.addEventListener("click", () => {
     let to = document.querySelector(el.dataset.element);
@@ -16,11 +14,7 @@ document.querySelector(".form").addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-(function(h,o,t,j,a,r){
-  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:885215,hjsv:6};
-  a=o.getElementsByTagName('head')[0];
-  r=o.createElement('script');r.async=1;
-  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-  a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-119483926-1');
