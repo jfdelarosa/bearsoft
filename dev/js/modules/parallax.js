9889c5772e32206bfe91e3c76661a7922f5912a1
Juan Fernando <jfdelarosa@outlook.com>
2018-05-17 19:46:08 -0500

    prallax only in pc

--- dev/js/modules/parallax.js
+++ dev/js/modules/parallax.js
@@ -3,13 +3,15 @@ export default () => {
     const offset = 200,
     constante = 2;
     let offsetY = window.pageYOffset + offset;
-    document.querySelectorAll(".section .container").forEach((el, index, array) => {
-      let limit = el.offsetTop + el.offsetHeight + offset;
-      if(offsetY > el.offsetTop && offsetY <= limit){
-        el.style.backgroundPositionY = "calc(1rem + " + (offsetY - el.offsetTop) / constante + "px)";
-      }else{
-        el.style.backgroundPositionY = "1rem";
-      }
-    });
+    if(window.matchMedia("(min-width: 1000px)").matches){
+      document.querySelectorAll(".section .container").forEach((el, index, array) => {
+        let limit = el.offsetTop + el.offsetHeight + offset;
+        if(offsetY > el.offsetTop && offsetY <= limit){
+          el.style.backgroundPositionY = "calc(1rem + " + (offsetY - el.offsetTop) / constante + "px)";
+        }else{
+          el.style.backgroundPositionY = "1rem";
+        }
+      });
+    }
   });
 };
